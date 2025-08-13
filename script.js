782fc6d2b9f27c4d87c061318cb6ee2bb991ae43
susan kandel 1 <148523013+Susankandel@users.noreply.github.com>
2025-08-13 22:50:00 +0545

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2983035174564415"
+     crossorigin="anonymous"></script>
